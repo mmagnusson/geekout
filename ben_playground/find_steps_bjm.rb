e0becfdb22521f386d0f0486a32bed4126ff9b9d
bemyhre <ben.myhre35@gmail.com>
2015-01-21 07:32:37 -0600

    first draft of working count producer

--- ben_playground/find_steps_bjm.rb
+++ ben_playground/find_steps_bjm.rb
@@ -5,7 +5,7 @@ end
 
 def first_number(arr)
 	if arr[0] == "-"
-		#DO THIS LATER
+		return ((arr[1][0].to_f / arr[1][2].to_f)*(-1.0)).round.to_i
 	else
 		(arr[0][0].to_f / arr[0][2].to_f).round.to_i
 	end
@@ -26,11 +26,6 @@ def string_to_rounded_complex_number(string_to_change)
 	return Complex(first,second)
 end
 
-def round_fraction(sign, number)
-
-end
-
-
 def complex_divide(complex_1, complex_2)
 	return (complex_1/complex_2)
 end
@@ -47,13 +42,14 @@ end
 #main
 def steps_to_ea(complex_1, complex_2)
 	count=0
-	remainder=1
+	remainder=nil
 	while remainder != 0
 		puts "%%%%%%%%%%%%%%%"
 		puts "Iteration number is " + (count+1).to_s
-		puts "Current remainder is (first one is not right) " + remainder.to_s
+		puts "Current remainder (first iteration it should be nil)" + remainder.to_s
 		puts "Current first number is " + complex_1.to_s
 		puts "Current second number is " + complex_2.to_s
+		puts "all math/assignment for this iteration will now happen"
 		puts "$$$$$$$$$$$$$$$$"
 		divided = complex_divide(complex_1, complex_2)
 		remainder = complex_1 - (string_to_rounded_complex_number(complex_to_string(divided)))*complex_2
@@ -61,6 +57,8 @@ def steps_to_ea(complex_1, complex_2)
 		complex_2=remainder
 		count+=1
 	end
+
+	puts "the method has identified the remainder as zero. The number of steps is:"
 	count
 end
 
@@ -69,7 +67,12 @@ end
 
 #puts string_to_rounded_complex_number(complex_to_string(complex_divide(Complex(3,-2), Complex(2,1))))
 
-steps_to_ea(Complex(3,-2),Complex(2,1))
+puts steps_to_ea(Complex(3,-2),Complex(2,1))
+
+#puts complex_divide(Complex(3,-2),Complex(2,1))
+#puts Complex(3,-2) - Complex(3,-1)
+
+#puts string_to_rounded_complex_number("4/5-7/5i")
 
 
 
